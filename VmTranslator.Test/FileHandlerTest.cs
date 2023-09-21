f126a7b8beb2630554dbd316bbe14fbe708ee513
anit3k <steffen.halberg@gmail.com>
2023-09-21 17:10:59 +0200

    Some small refactor along with updated NUnit test

--- VmTranslator.Test/FileHandlerTest.cs
+++ VmTranslator.Test/FileHandlerTest.cs
@@ -12,45 +12,64 @@ namespace VmTranslator.Test
         }
 
         [Test]
-        public void ReadAllLines_ValidFile_ReturnsList()
+        [TestCase("AddSimple.vm")]
+        [TestCase("valid_vm_file.vm")]
+        public void ReadAllLines_ValidFile_ReturnsList(string path)
         {
             // Arrange
-            string filePath = "valid_vm_file";
-            File.WriteAllLines($"{filePath}.vm", new[] { "line1", "line2", "line3" });
+            File.WriteAllLines($"{path}", new[] { "push constant 66", "@sp", "add" });
 
             // Act
-            List<string> lines = _fileHandler.ReadAllLines(filePath);
+            List<string> lines = _fileHandler.ReadAllLines(path);
 
             // Assert
             Assert.AreEqual(3, lines.Count);
-            Assert.AreEqual("line1", lines[0]);
-            Assert.AreEqual("line2", lines[1]);
-            Assert.AreEqual("line3", lines[2]);
+            Assert.AreEqual("push constant 66", lines[0]);
+            Assert.AreEqual("@sp", lines[1]);
+            Assert.AreEqual("add", lines[2]);
         }
 
         [Test]
-        public void SaveAllLines_ValidFile_SavesFile()
+        [TestCase("AddSimple.txt")]
+        [TestCase("valid_vm_file.bin")]
+        public void ReadAllLines_NotValidFile_ThrowsException(string path)
+        {
+            // Arrange: Create the test file with the specified name
+            File.WriteAllText(path, "This is not a valid VM file content");
+
+            // Act & Assert: Use the ExpectedException attribute to specify the expected exception
+            Assert.Throws<IOException>(() => _fileHandler.ReadAllLines(path), "File is not found or the file is not of type .vm");
+        }
+
+        [Test]
+        [TestCase("AddSimple.vm")]
+        [TestCase("valid_vm_file.vm")]
+        public void SaveAllLines_ValidFile_SavesFile(string path)
         {
             // Arrange
-            string filePath = "valid_asm_file";
             List<string> linesToSave = new List<string> { "asmLine1", "asmLine2" };
 
             // Act
-            _fileHandler.SaveAllLines(filePath, linesToSave);
+            _fileHandler.SaveAllLines(path, linesToSave);
+
+            // Calculate the expected ASM file path based on the VM file path
+            string expectedAsmPath = Path.ChangeExtension(path, "asm");
 
             // Assert
-            Assert.IsTrue(File.Exists($"{filePath}.asm"));
-            string[] savedLines = File.ReadAllLines($"{filePath}.asm");
+            Assert.IsTrue(File.Exists(expectedAsmPath));
+            string[] savedLines = File.ReadAllLines(expectedAsmPath);
             Assert.AreEqual(2, savedLines.Length);
             Assert.AreEqual("asmLine1", savedLines[0]);
             Assert.AreEqual("asmLine2", savedLines[1]);
         }
+
 
         [TearDown]
         public void Cleanup()
         {
             // Clean up any created files after each test
             File.Delete("valid_vm_file.vm");
+            File.Delete("AddSimple.vm");
             File.Delete("valid_asm_file.asm");
         }
     }
