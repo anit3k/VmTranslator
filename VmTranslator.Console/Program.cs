7256f5a20ec29e399301c33e16337c15be139bd3
Mads Tinko Madsen <Lyderslev1@gmail.com>
2023-09-21 11:42:53 +0200

    Setup file read, and write functionality.

--- VmTranslator.Console/Program.cs
+++ VmTranslator.Console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VmTranslator.Console;
+
+FileHandler fileHandler = new FileHandler();
+
+Console.WriteLine("Please enter name of the file you wish to translate.");
+
+string fileName = Console.ReadLine();
+
+string filePath = $"C:\\Files\\{fileName}";
+
+try
+{
+	List<string> lines = fileHandler.ReadAllLines(filePath);
+
+	fileHandler.SaveAllLines(filePath, lines);
+}
+catch (Exception msg)
+{
+	Console.WriteLine(msg.Message);
+}
+
+Console.WriteLine("Program terminated");
